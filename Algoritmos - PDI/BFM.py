e3f8bb3e6e88003d4a9e076313b6f652e74ea70a
Juanlavagnini <91229986+Juanlavagnini@users.noreply.github.com>
2023-06-18 11:06:16 -0300

    BFM

--- Algoritmos - PDI/BFM.py
+++ Algoritmos - PDI/BFM.py
@@ -10,26 +10,27 @@ def BFM(Grafo, salida):
 
     while Q:
         for vecino in Grafo[Q[0]]:
-            if dist[vecino] > dist[Q[0]] + Grafo[Q[0]][vecino]:
+            if dist[vecino] > dist[Q[0]] + Grafo[Q[0]][vecino] and (Q[0]!=prev[vecino] and prev[Q[0]]!=vecino):
                 dist[vecino] = dist[Q[0]] + Grafo[Q[0]][vecino]
                 prev[vecino] = Q[0]
-            if vecino not in Q:
-                Q.append(vecino)
+                if vecino not in Q:
+                    Q.append(vecino)
         Q.remove(Q[0])
     print(Q)
     return result
 
+
 grafo = {
     '1': {'3': 1, '6': 2},
-    '2': {'9': 1},
-    '3': {'4': 2, '2': 2},
-    '4': {'5': 3},
-    '5': {'10': 4},
-    '6': {'7': 2, '2': 1},
-    '7': {'9': 3, '11': 2},
-    '9': {'10': 2, '11': 2},
-    '10': {},
-    '11': {}
+    '2': {'9': 1, '3': 2, '6': 1},
+    '3': {'4': 2, '2': 2, '1': 1},
+    '4': {'5': 3, '3': 2},
+    '5': {'10': 4, '4': 3},
+    '6': {'7': 2, '2': 1, '1': 2},
+    '7': {'9': 3, '11': 2, '6': 2},
+    '9': {'10': 2, '11': 2, '7': 3, '2': 1},
+    '10': {'5': 4, '9': 2},
+    '11': {'7': 2, '9': 2}
 }
 
 def main():
@@ -41,7 +42,7 @@ def main():
                 nexthop = "directo"
             else:
                 nexthop = s[1][destino]
-            print("{:<14s} {:<10s} {:<10s} {:<10s}".format("", destino, nexthop, str(s[1][destino])))
+            print("{:<14s} {:<10s} {:<10s} {:<10d}".format("", destino, nexthop, s[2][destino]))
 
 
 main()
